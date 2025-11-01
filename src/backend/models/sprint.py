39ae3f398d076154980bd3b59d13d8c58378cb15
Jérémy MARTIN <jeremy.delpech.martin@gmail.com>
2025-11-01 23:47:44 +0100

    Add: sprint_repository Add: Sprint.Id in models
    Signed-off-by: Jérémy MARTIN <jeremy.delpech.martin@gmail.com>

--- src/backend/models/sprint.py
+++ src/backend/models/sprint.py
@@ -1,9 +1,10 @@
-from datetime import datetime
+from datetime import date
 from pydantic import BaseModel
 
 class Sprint(BaseModel):
+    id: int
     tfs_number: str
-    name: str
-    starting_date: datetime
-    ending_date: datetime
+    code: str
+    starting_date: date
+    ending_date: date
     
---
+++ src/backend/repositories/sprint_repository.py
@@ -0,0 +1,99 @@
+from src.backend.database.db import get_connection
+from src.backend.models.sprint import Sprint
+from datetime import date
+
+class SprintRepository:
+    """How to deal with Sprint table"""
+
+    def find_by_id(self, sprint_id: int) -> Sprint | None:
+        conn = get_connection()
+        result = conn.execute("""
+        SELECT id, tfs_number, code, starting_date, ending_date FROM Sprints WHERE Id = ?
+        """, (sprint_id,)).fetchone()
+        conn.close()
+        if not result:
+            return None
+        _id, _tfs_number, _code, _starting_date, _ending_date = result
+        return Sprint(
+            id=_id,
+            tfs_number=_tfs_number,
+            code=_code,
+            starting_date=_starting_date,
+            ending_date=_ending_date
+        )
+
+    def find_by_tfs(self, tfs_number: int) -> Sprint | None:
+        conn = get_connection()
+        result = conn.execute("""
+        SELECT id, tfs_number, code, starting_date, ending_date FROM Sprints WHERE tfs_number = ?
+        """, (tfs_number,)).fetchone()
+        conn.close()
+        if not result:
+            return None
+        _id, _tfs_number, _code, _starting_date, _ending_date = result
+        return Sprint(
+            id=_id,
+            tfs_number=_tfs_number,
+            code=_code,
+            starting_date=_starting_date,
+            ending_date=_ending_date
+        )
+
+    def find_all(self) -> list[Sprint] | None:
+        conn = get_connection()
+        results = conn.execute("""
+        SELECT id, tfs_number, code, starting_date, ending_date FROM Sprints
+        """).fetchall()
+        conn.close()
+        if not results:
+            return None
+        return [
+            Sprint(
+                id=r[0],
+                tfs_number=r[1],
+                code=r[2],
+                starting_date=r[3],
+                ending_date=r[4]
+            )
+            for r in results
+        ]
+
+    def find_active(self, today: date = None) -> Sprint | list[Sprint] | None:
+        """Récupère les sprints actifs (aujourd'hui entre starting_date et ending_date)"""
+        if today is None:
+            today = date.today()
+
+        conn = get_connection()
+        results = conn.execute(
+            "SELECT id, tfs_number, code, starting_date, ending_date FROM Sprints WHERE starting_date <= ? AND ending_date >= ?",
+            (today, today)
+        ).fetchall()
+        conn.close()
+        if not results:
+            return None
+        if len(results) == 1:
+            _id, _tfs_number, _code, _starting_date, _ending_date = results[0]
+            return Sprint(
+                id=_id,
+                tfs_number=_tfs_number,
+                code=_code,
+                starting_date=_starting_date,
+                ending_date=_ending_date
+            )
+        return [
+            Sprint(
+                id=r[0],
+                tfs_number=r[1],
+                code=r[2],
+                starting_date=r[3],
+                ending_date=r[4]
+            ) for r in results
+        ]
+
+    def create_sprint(self, tfs_number: int, code: str, starting_date: date, ending_date: date):
+        conn = get_connection()
+        conn.execute("""
+        INSERT INTO Sprints (tfs_number, code, starting_date, ending_date)
+            VALUES (?, ?, ?, ?)
+        """, (tfs_number, code, starting_date, ending_date))
+        conn.close()
